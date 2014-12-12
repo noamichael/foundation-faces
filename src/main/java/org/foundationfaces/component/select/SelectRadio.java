d721a1802c4ef6641a32ab67d6dbe3b50865f223
Michael <Michael@Michael-PC>
2014-12-11 23:17:55 -0500

    Client side behavior for the SelectRadio!

--- src/main/java/org/foundationfaces/component/select/SelectRadio.java
+++ src/main/java/org/foundationfaces/component/select/SelectRadio.java
@@ -12,6 +12,7 @@ import org.foundationfaces.component.Foundation;
  */
 @FacesComponent(SelectRadio.MY_COMPONENT_TYPE)
 @ResourceDependencies({
+    @ResourceDependency(library = "javax.faces", name = "jsf.js"),
     @ResourceDependency(name = "/foundationfaces/css/foundation.min.css"),
     @ResourceDependency(name = "/foundationfaces/css/normalize.css"),
     @ResourceDependency(name = "/foundationfaces/js/vendor/jquery.js"),
@@ -48,9 +49,58 @@ public class SelectRadio extends HtmlSelectOneRadio {
         getStateHelper().put(PropertyKeys.containerStyleClass, containerStyleClass);
     }
 
+    /**
+     * @return the small
+     */
+    public String getSmall() {
+        return (String) getStateHelper().eval(PropertyKeys.small, "2");
+    }
+
+    /**
+     * @param small the small to set
+     */
+    public void setSmall(String small) {
+        getStateHelper().put(PropertyKeys.small, small);
+    }
+
+    /**
+     * @return the medium
+     */
+    public String getMedium() {
+        return (String) getStateHelper().eval(PropertyKeys.medium, "4");
+    }
+
+    /**
+     * @param medium the medium to set
+     */
+    public void setMedium(String medium) {
+        getStateHelper().put(PropertyKeys.medium, medium);
+    }
+
+    /**
+     * @return the large
+     */
+    public String getLarge() {
+        return (String) getStateHelper().eval(PropertyKeys.large, "6");
+    }
+
+    /**
+     * @param large the large to set
+     */
+    public void setLarge(String large) {
+        getStateHelper().put(PropertyKeys.large, large);
+    }
+
+    public String getColumnStyleClasses() {
+        String small = "small-" + getSmall();
+        String medium = "medium-" + getMedium();
+        String large = "large-" + getLarge();
+        return "columns" + " " + small + " " + medium + " " + large + " ";
+    }
+
     protected enum PropertyKeys {
 
-        containerStyleClass;
+        containerStyleClass, small, medium, large;
     }
 
 }
--- src/main/java/org/foundationfaces/component/select/SelectRadioRenderer.java
+++ src/main/java/org/foundationfaces/component/select/SelectRadioRenderer.java
@@ -1,14 +1,18 @@
 package org.foundationfaces.component.select;
 
 import java.io.IOException;
+import java.util.List;
 import javax.faces.component.UIComponent;
-import javax.faces.component.html.HtmlSelectOneRadio;
+import javax.faces.component.behavior.ClientBehavior;
+import javax.faces.component.behavior.ClientBehaviorContext;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
+import javax.faces.convert.Converter;
+import javax.faces.model.SelectItem;
 import javax.faces.render.FacesRenderer;
 import javax.faces.render.Renderer;
+import org.foundationfaces.component.ComponentUtil;
 import org.foundationfaces.component.Foundation;
-import org.foundationfaces.component.WrappedRenderer;
 import org.foundationfaces.util.StringUtil;
 
 /**
@@ -16,38 +20,88 @@ import org.foundationfaces.util.StringUtil;
  * @author Michael
  */
 @FacesRenderer(componentFamily = Foundation.COMPONENT_FAMILY, rendererType = SelectRadio.MY_RENDERER_TYPE)
-public class SelectRadioRenderer extends WrappedRenderer {
+public class SelectRadioRenderer extends Renderer {
 
     @Override
     public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
         ResponseWriter writer = context.getResponseWriter();
         SelectRadio select = (SelectRadio) component;
+        List<SelectItem> selectItems = ComponentUtil.getSelectItems(select);
+        int i = 0;
         writer.startElement("div", component);
-        String containerStyleClass = select.getContainerStyleClass();
-        if (!StringUtil.isNullOrEmpty(containerStyleClass)) {
-            writer.writeAttribute("class", SelectRadio.STYLE_CLASS + " " + containerStyleClass, null);
-        } else {
-            writer.writeAttribute("class", SelectRadio.STYLE_CLASS, null);
+        writer.writeAttribute("class", "row", null);
+        writer.writeAttribute("id", select.getClientId(context), null);
+        for (SelectItem item : selectItems) {
+            encodeSingleRadio(context,
+                    select,
+                    item,
+                    i,
+                    writer);
+            i++;
         }
-        getDefaultRenderer(context).encodeBegin(context, component);
-    }
-
-    @Override
-    public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
-        ResponseWriter writer = context.getResponseWriter();
-        SelectRadio select = (SelectRadio) component;
-        getDefaultRenderer(context).encodeEnd(context, component);
-        writer.startElement("label", null);
-        writer.writeAttribute("for", select.getClientId(context), null);
-        writer.endElement("label");
         writer.endElement("div");
+
+    }
+
+    protected void encodeSingleRadio(FacesContext context,
+            SelectRadio select,
+            SelectItem selectItem,
+            int i,
+            ResponseWriter writer) throws IOException {
+        if (selectItem != null) {
+            String clientId = select.getClientId(context);
+            String styleClass = select.getStyleClass();
+            styleClass = StringUtil.isNullOrEmpty(styleClass) ? "" : styleClass;
+            writer.startElement("div", null);
+            writer.writeAttribute("class", select.getColumnStyleClasses() + SelectRadio.STYLE_CLASS, null);
+            writer.startElement("input", null);
+            writer.writeAttribute("name", clientId, null);
+            Object value = select.getValue();
+            if (value != null && value.equals(selectItem.getValue())) {
+                writer.writeAttribute("checked", "checked", null);
+            }
+            writer.writeAttribute("type", "radio", null);
+            writer.writeAttribute("id", clientId + ":" + i, clientId);
+            writer.writeAttribute("value", String.valueOf(selectItem.getValue()), null);
+            encodeEvents(context, select, writer);
+            writer.writeAttribute("class", styleClass, null);
+            writer.endElement("input");
+            writer.startElement("label", null);
+            writer.writeAttribute("for", clientId + ":" + i, null);
+            writer.endElement("label");
+            writer.endElement("div");
+        }
+    }
+
+    protected void encodeEvents(FacesContext context,
+            SelectRadio select,
+            ResponseWriter writer) throws IOException {
+        List<ClientBehavior> changeBehaviors = select.getClientBehaviors().get("change");
+        if (changeBehaviors != null && !changeBehaviors.isEmpty()) {
+            ClientBehaviorContext clientBehaviorContext
+                    = ClientBehaviorContext.createClientBehaviorContext(context,
+                            select, "change", select.getClientId(context), null);
+            StringBuilder builder = new StringBuilder();
+            for (ClientBehavior behavior : changeBehaviors) {
+                builder.append(behavior.getScript(clientBehaviorContext));
+                builder.append(';');
+            }
+            writer.writeAttribute("onChange", builder.toString(), null);
+        }
     }
 
     @Override
-    protected Renderer getDefaultRenderer(FacesContext context) {
-        return context.getRenderKit()
-                .getRenderer(HtmlSelectOneRadio.COMPONENT_FAMILY,
-                        "javax.faces.Radio");
+    public void decode(FacesContext context, UIComponent component) {
+        SelectRadio selectRadio = (SelectRadio) component;
+        String value = context.getExternalContext().
+                getRequestParameterMap().get(selectRadio.getClientId(context));
+        Converter converter = selectRadio.getConverter();
+        if (converter != null) {
+            Object convertedValue = converter.getAsObject(context, component, value);
+            selectRadio.setSubmittedValue(convertedValue);
+        } else {
+            selectRadio.setSubmittedValue(value);
+        }
     }
 
 }
